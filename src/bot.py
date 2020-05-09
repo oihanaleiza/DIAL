6535cb861ac4f3f159f66aeba7e33e95fb9a70ec
oihanaleiza <oihanaleiza@gmail.com>
2020-05-09 18:51:48 +0200

    gehituta

--- src/bot.py
+++ src/bot.py
@@ -15,6 +15,7 @@ parser.add_argument('-decoding_strategy', type=str, default='top1', choices=['to
 
 args = parser.parse_args()
 
+# Dekodetzea
 def decode(logits, decoding_strategy='max', k=3, temp=0.4):
   if decoding_strategy=='top1':
     target = logits.max(1)[1]
@@ -24,6 +25,7 @@ def decode(logits, decoding_strategy='max', k=3, temp=0.4):
     target = torch.multinomial(logits.squeeze().div(temp).exp().cpu(), 1)
   return target
 
+# Ebaluazioa
 def evaluate(sentence):
   with torch.no_grad():
     sentence = '<sos> ' + sentence + ' <eos>'
@@ -43,18 +45,49 @@ def evaluate(sentence):
         output_sentence = output_sentence + ' ' + word
   return output_sentence
 
+def evaluate2(sentence):
+  with torch.no_grad():
+    sentence = '<sos> ' + sentence + ' <eos>'
+    sent_len = len(sentence.split())
+    sentence = torch.Tensor([text_fielden.vocab.stoi[i] for i in sentence.lower().split()]).long().view(sent_len, 1)
+    target = torch.Tensor([text_fielden.vocab.stoi['<sos>']]).long()
+    output_sentence = ''
+    encoder_outputs, hidden = modelen.encoder(sentence)
+    for t in range(MAX_LENGTH):
+    # first input to the decoder is the <sos> token
+      output, hidden = modelen.decoder(target, hidden, encoder_outputs)
+      target = decode(output, decoding_strategy)
+      word = text_fielden.vocab.itos[target.numpy()[0]]
+      if word == '<eos>':
+        return output_sentence
+      else:
+        output_sentence = output_sentence + ' ' + word
+  return output_sentence
+
 #Load model and fields
 text_field = torch.load('../modeleu/text_field.Field')
 model = torch.load('../modeleu/model.pt', map_location=torch.device('cpu'))
+text_fielden = torch.load('../model/text_field.Field')
+modelen = torch.load('../model/model.pt', map_location=torch.device('cpu'))
 torch.nn.Module.dump_patches = True
 MAX_LENGTH = 10
 
+
 #Main system loop
 model.eval()
+modelen.eval()
 decoding_strategy = args.decoding_strategy
 
+hizkuntza = "eu"
 
-TOKEN = "1082764579:AAGTX1fWL9g5PwRye-geOK7hc3HiOv8tHq0"
+def erantzun(message):
+    if hizkuntza=="eu":
+        return evaluate(' '.join(_basic_english_normalize(message.text)))
+    else:
+        return evaluate2(' '.join(_basic_english_normalize(message.text)))
+
+
+TOKEN = ""
 bot = telebot.TeleBot(token=TOKEN)
 
 @bot.message_handler(commands=['start']) # Ongi etorri mezua
@@ -63,11 +96,27 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['help']) # Laguntza
 def send_welcome(message):
-    bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
+    reply = "Hizkuntza aukeratzeko:\n /1 Euskara \n /2 Ingelesa "
+    bot.send_message(chat_id=message.chat.id, text=reply)
+
+@bot.message_handler(commands=['1']) # Euskara aukeraketa
+def send_welcome(message):
+    bot.reply_to(message, 'Euskara aukeratuta')
+    hizkuntza ="eu"
+
+@bot.message_handler(commands=['2']) # Ingelesa aukeraketa
+def send_welcome(message):
+    bot.reply_to(message, 'Ingelesa aukeratuta')
+    hizkuntza="en"
+
 
 @bot.message_handler(func=lambda msg: msg.text is not None)
-def echo(message):
-    sentence = evaluate(' '.join(_basic_english_normalize(message.text)))
+def talk(message):
+    if hizkuntza=="eu":
+        print("sartuta")
+        sentence = evaluate(' '.join(_basic_english_normalize(message.text)))
+    else:
+        sentence = evaluate2(' '.join(_basic_english_normalize(message.text)))
     print('-' + sentence.strip().capitalize())
     reply = sentence.strip().capitalize()
     bot.send_message(chat_id=message.chat.id, text=reply)
